dd31ec733416662ce9f52429c2c306fd7678917b
Aman kumar Huriya <106256434+CoolAman123@users.noreply.github.com>
2022-07-13 14:54:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState ,useEffect} from 'react';
 import './App.css';
 
+const url=`https://pokeapi.co/api/v2/pokemon`;
+
 function App() {
+  const [pokeData, setPokeData]= useState([]);
+  const[nextUrl,setNextUrl]= useState();
+  const[prevUrl,setPrevUrl]= useState();
+
+
+  const List = async () => {
+    try {
+
+      const searchRes = await fetch(url);
+      const searchData = await searchRes.json();
+      setNextUrl(searchData.next);
+      setNextUrl(searchData.previous);
+      getPokemon(searchData.results)  
+      // console.log(searchData.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getPokemon=async(res)=>{
+    res.map(async(item)=>{
+      const result= await fetch(item.url);
+      const finalData=(result.url);
+      console.log(finalData);
+    })
+  }
+
+     useEffect(() => {
+      List();
+     }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search"></div>
+      <div className="container">
+      <div className="left-container"></div>
+      <div className="right-container"></div>
+      </div>
     </div>
   );
 }
